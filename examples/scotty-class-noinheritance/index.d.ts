0dbdf92ab4440a7de7460d7ee117029423d44a76
Manuel Serrano <Manuel.Serrano@inria.fr>
2022-07-21 09:30:42 +0200

    Fix type declaration

--- examples/scotty-class-noinheritance/index.d.ts
+++ examples/scotty-class-noinheritance/index.d.ts
@@ -1,16 +1,9 @@
- export class Point {
+export class Point {
  x:number;
  y:number;
    
- constructor(x:number, y:number) {
-      this.x = x;
-      this.y = y;
-   }
-   
-   toStr(): string {
-      return 0;
-   }
+ constructor(x:number, y:number);
+ toStr(): string;
 }
 
-export function Point(): string;
 export function shift(p:Point): Point;
