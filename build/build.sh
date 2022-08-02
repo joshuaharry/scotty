9638c1bf6d547013ecd9768d1c4f4eb0da75f8e9
Robby Findler <robby@racket-lang.org>
2022-08-01 21:00:29 -0500

    Revert "skip the history of definitely typed to speed up the setup"
    This reverts commit 91e6a233e7707bb579dc6e2e20b83a07e83fc023.
    
    We need the whole history, apparently

--- build/build.sh
+++ build/build.sh
@@ -8,5 +8,5 @@ cp ./__contracts__/contract-proxy-mode.js ~/.scotty/contracts
 printf "%s\n" "998fe1077af548a7c97fcee5f2057bdb04d3855c" > ~/.scotty/commit
 cd ~/.scotty || exit 1
 if [ ! -d ~/.scotty/DefinitelyTyped ]; then
-  git clone --depth 1 https://github.com/DefinitelyTyped/DefinitelyTyped
+  git clone https://github.com/DefinitelyTyped/DefinitelyTyped
 fi
